04b1e34ddbccfab667babcf4c2188c9e98176a78
Jacob Bieker <jacob@bieker.us>
2015-05-09 22:35:51 -0700

    Added Standard Deviation and DeltaX, Y

--- brownian.py
+++ brownian.py
@@ -7,6 +7,38 @@ import math
 bead_size = 3.2 #um
 temp = 23.1 #C
 
+'''
+Basic functions to call later
+'''
+
+
+def quad_error(b_error, v_error):
+    return math.sqrt((b_error ** 2) + (v_error ** 2))
+
+
+def standard_dev(set_of_values, avg_value):
+    #Finds Standard Deviation for V
+    diffsquared = 0
+    sum_diffsquared = 0
+    std_dev = 0
+    for value in set_of_values:
+        diffsquared = (value-avg_value)**2
+        sum_diffsquared += diffsquared
+        std_dev = (((sum_diffsquared)/(len(set_of_values) - 1))**(1/2))
+    return std_dev
+
+
+def kB(D, f, T):
+    return (D * f) / T
+
+
+def f(R):
+    return 6 * math.pi * 1 * (10 ^ (-3)) * R
+
+
+def D(delta_t, avg_axis_2):
+    return avg_axis_2 / (2 * delta_t)
+
 
 '''
 Calibrating the Scale per pixel
@@ -39,7 +71,7 @@ print cal_y
 
 x_scale = pythag(.32, float(cal_x[1]) - float(cal_x[0]))
 
-y_scale = pythag(0, float(cal_y[3]) - float(cal_y[2]))
+y_scale = pythag(0, float(cal_y[1]) - float(cal_y[0]))
 
 print x_scale
 print y_scale
@@ -55,6 +87,9 @@ Converting the pixel values in data to the calibrated data
 
 data_file = 'data.txt'
 
+'''
+Data analysis for Delta x, y
+'''
 x_values = []
 y_values = []
 
@@ -91,3 +126,64 @@ pyplot.show()
 pyplot.hist(delta_y_val)
 pyplot.show()
 
+
+#Mean and standard deviation for deltaX and deltaY
+mean_delta_x = 0
+mean_delta_y = 0
+
+for value in delta_x_val:
+    mean_delta_x += value
+mean_delta_x /= len(delta_x_val)
+
+for value in delta_y_val:
+    mean_delta_y += value
+mean_delta_y /= len(delta_y_val)
+
+standard_dev_x = standard_dev(delta_x_val, mean_delta_x)
+standard_dev_y = standard_dev(delta_y_val, mean_delta_y)
+
+print ("Mean X: " + str(mean_delta_x) + " SD: " + str(standard_dev_x))
+print ("Mean Y: " + str(mean_delta_y) + " SD: " + str(standard_dev_y))
+
+#End Standard deviation and mean for deltaX and deltaY
+
+#Mean and standard deviation for deltaX^@ and deltaY^2
+mean_delta_x_2 = 0
+mean_delta_y_2 = 0
+
+delta_x_2_val = []
+delta_y_2_val = []
+
+for value in delta_x_val:
+    delta_x_2_val.append((value ** 2))
+
+for value in delta_y_val:
+    delta_y_2_val.append((value ** 2))
+
+
+for value in delta_x_2_val:
+    mean_delta_x_2 += value
+mean_delta_x_2 /= len(delta_x_val)
+
+for value in delta_y_2_val:
+    mean_delta_y_2 += value
+mean_delta_y_2 /= len(delta_y_val)
+
+standard_dev_x_2 = standard_dev(delta_x_val, mean_delta_x_2)
+standard_dev_y_2 = standard_dev(delta_y_val, mean_delta_y_2)
+
+print ("Mean X^2: " + str(mean_delta_x_2) + " SD: " + str(standard_dev_x_2))
+print ("Mean Y^2: " + str(mean_delta_y_2) + " SD: " + str(standard_dev_y_2))
+
+'''
+End Delta X, y analysis
+'''
+
+'''
+Calculating D
+'''
+
+r_2 = []
+
+for index, value in enumerate(x_values):
+    r_2.append((value ** 2) + (y_values[index] ** 2))
